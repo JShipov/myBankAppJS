3aba20778a952fd2e992091a1656dbd2b3d8c79e
Iuliia <107051542+JShipov@users.noreply.github.com>
2023-01-26 14:27:54 +0400

    Update script.js

--- script.js
+++ script.js
@@ -237,32 +237,3 @@ const { deposits, withdrawals } = accounts
     { deposits: 0, withdrawals: 0 }
   );
 
-console.log(deposits, withdrawals);
-
-const convertTitleCase = function (title) {
-  const capitzalize = (str) => str[0].toUpperCase() + str.slice(1);
-
-  const exceptions = ["a", "an", "and", "the", "but", "or", "on", "in", "with"];
-
-  const titleCase = title
-    .toLowerCase()
-    .split(" ")
-    .map((word) => (exceptions.includes(word) ? word : capitzalize(word)))
-    .join(" ");
-
-  return capitzalize(titleCase);
-};
-
-console.log(convertTitleCase("this is a nice title"));
-console.log(convertTitleCase("this is a LONG title but not too long"));
-console.log(convertTitleCase("and here is another title with an EXAMPLE"));
-
-const checkEatingOkay = (dog) =>
-  dog.curFood > dog.recFood * 0.9 && dog.curFood < dog.recFood * 1.1;
-
-console.log(dogs.some(checkEatingOkay));
-
-console.log(dogs.filter(checkEatingOkay));
-
-const dogsSorted = dogs.slice().sort((a, b) => a.recFood - b.recFood);
-console.log(dogsSorted);
